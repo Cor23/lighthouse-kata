bfcaf007c0f9d9e5d40382f1808d0a27f1c01af1
corey <corey.cy.young@gmail.com>
2022-06-24 23:13:53 +0000

    Completed kata18.js

--- kata18.js
+++ kata18.js
@@ -1,5 +1,31 @@
 const squareCode = function(message) {
-  // Put your solution here
+  let msgSpcRmv = message.replaceAll(" ", "");
+  let msgLength = msgSpcRmv.length;
+  let sqrNum = Math.ceil(Math.sqrt(msgLength));
+  let sqrString = "";
+  let output = "";
+  
+  for(let i = 0; i < sqrNum; i++){
+    if(i === sqrNum){
+      sqrString += msgSpcRmv.substr(0, sqrNum);
+    } else {
+      sqrString += msgSpcRmv.substr(0, sqrNum) + "\n";
+      msgSpcRmv = msgSpcRmv.substr(sqrNum);
+    }
+  }
+  
+  sqrString = sqrString.trim();
+  sqrString = sqrString.split("\n");
+  
+  for(let j = 0; j < sqrString[0].length; j++){
+    for(let k = 0; k < sqrString.length; k++){
+      if(sqrString[k][j]){
+      output += sqrString[k][j];
+      }
+    }
+    output += " ";
+  }
+  return output;
 };
 
 console.log(squareCode("chill out"));
