8ebb90c1076806b56864776ba5447169570f3bb5
Corey <corey.cy.young@gmail.com>
2022-06-27 16:39:03 -0700

    Completed kata21.

--- kata21/kata21.js
+++ kata21/kata21.js
@@ -1,5 +1,36 @@
 let prompt = require("prompt-sync")();
+let answer = Math.ceil(Math.random() * 10);
+let guess = prompt("Guess a number: ");
+guess = Number(guess);
+let previousGuess;
 
-// code below (replace this example)
-let answer = prompt("Guess a number: ");
-console.log("You answered: " + answer);
+for(let i = 0; i < 100; i++){
+  if(isNaN(guess) === true){
+    i--;
+    console.log("Not a number! Try again!");
+    previousGuess = guess;
+    guess = prompt("Guess a number: ");
+    guess = Number(guess);
+  } else if(guess === previousGuess){
+    i--;
+    console.log("Already Guessed!");
+    previousGuess = guess;
+    guess = prompt("Guess a number: ");
+    guess = Number(guess);
+  } else if(guess < answer){
+    console.log("Too Low!");
+    previousGuess = guess
+    guess = prompt("Guess a number: ");
+    guess = Number(guess);
+  } else if(guess > answer){
+    console.log("Too High!");
+    previousGuess = guess
+    guess = prompt("Guess a number: ");
+    guess = Number(guess);
+  } else if(guess === answer){
+    console.log("You got it! You took " + (i+1) + " attempts!");
+    break;
+  }
+};
+
+
