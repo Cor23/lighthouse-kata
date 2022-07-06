99024349cb1d8ada462721fee6092ad72868f5a4
Corey <corey.cy.young@gmail.com>
2022-07-06 15:58:48 -0700

    Direction logic added, output: INCORRECT

--- kata20.js
+++ kata20.js
@@ -1,11 +1,11 @@
 const blocksAway = function(directions) {
   let output = {"east": 0, "north": 0};
   const compass = ["north", "east", "south", "west"];
-  let facingDir = compass[0];
+  let facingDir;
   let dir = [];
   let steps = [];
 
-  //seperates array directions into seperate arrays dir, steps.
+//seperates array directions into seperate arrays dir, steps.
   for(let i = 0; i < directions.length; i++){
     switch(isNaN(directions[i])){
       case true:
@@ -16,40 +16,91 @@ const blocksAway = function(directions) {
     }
   };
 //-------------------------------------------------------------//
-  for(let j = 0; j < steps.length; j++){
-  //NORTH
+  for(let j = 0; j < dir.length; j++){
+    // set starting direction.
+    if(dir[0] === 'right' && j === 0){
+      facingDir = compass[1];
+      output['east'] += steps[j];
+      j++;
+    } else if(dir[0] === 'left' && j === 0){
+      facingDir = compass[0];
+      output['north'] += steps[j];
+      j++;
+    };
+
+    //NORTH
     if(facingDir === compass[0]){
       switch(dir[j]){
-        case "right":
-          facingDir = compass[1]
-          output["east"] = +steps[j];
+        case 'right':
+          facingDir = compass[1];
+          output['east'] += steps[j];
+          j++;
+          console.log(output, "NORTH-R")
           break;
-        case "left":
+        case 'left':
           facingDir = compass[3];
-          output["east"] = -steps[j];
+          output['east'] -= steps[j];
+          j++;
+          console.log(output, "NORTH-L")
           break;
       }
     };
-  //EAST
+
+    //EAST
     if(facingDir === compass[1]){
       switch(dir[j]){
-
+        case 'right':
+          facingDir = compass[2];
+          output['north'] -= steps[j];
+          j++;
+          console.log(output, "EAST-R")
+          break;
+        case 'left':
+          facingDir = compass[0];
+          output['north'] += steps[j];
+          j++;
+          console.log(output, "EAST-L")
+          break;
       }
     };
-  //SOUTH
+
+    //SOUTH
     if(facingDir === compass[2]){
       switch(dir[j]){
-
+        case 'right':
+          facingDir = compass[3];
+          output['east'] -= steps[j];
+          j++;
+          console.log(output, "SOUTH-R")
+          break;
+        case 'left':
+          facingDir = compass[1];
+          output['east'] += steps[j];
+          j++;
+          console.log(output, "SOUTH-L")
+          break;
       }
     };
-  //WEST
+
+    //WEST
     if(facingDir === compass[3]){
       switch(dir[j]){
-
+        case 'right':
+          facingDir = compass[0];
+          output['north'] += steps[j];
+          j++;
+          console.log(output, "WEST-R")
+          break;
+        case 'left':
+          facingDir = compass[2];
+          output['north'] -= steps[j];
+          j++;
+          console.log(output, "WEST-L")
+          break;
       }
     };
-    
-  };
+
+  }
 //-------------------------------------------------------------//
   console.log(dir);
   console.log(steps);
