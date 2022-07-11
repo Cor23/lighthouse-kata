5c1200a8e91503b757b3c9acaa5ba33123ad7688
Corey <corey.cy.young@gmail.com>
2022-07-11 14:31:16 -0700

    Restart kata19.js

--- kata19.js
+++ kata19.js
@@ -1,22 +1,5 @@
-let generateBoard = function(whiteQueen, blackQueen){
-  let chessBoard = [];
-  for(let i = 0; i < 8; i++){
-    let row = [];
-    chessBoard.push(row);
-    for(let j = 0; j < 8; j++){
-      if(whiteQueen[0] === i && whiteQueen[1] === j || blackQueen[0] === i && blackQueen[1] === j){
-        row.push(1);
-      } else {
-        row.push(0);
-      }
-    }
-  }
-  console.table(chessBoard);
-};
-
-
 let whiteQueen = [0, 5];
 let blackQueen = [5, 0];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
 console.log(generatedBoard);
-// console.log(queenThreat(generatedBoard));
+console.log(queenThreat(generatedBoard));
