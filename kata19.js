16c914701c00b911f9dbd483ca9747f1af54ddd0
Corey <corey.cy.young@gmail.com>
2022-07-12 10:07:44 -0700

    diagonal logic still required

--- kata19.js
+++ kata19.js
@@ -1,3 +1,41 @@
+let generateBoard = function(whiteQueen, blackQueen){
+  let chessBoard = [];
+  for(let i = 0; i < 8; i++){
+    let row = [];
+    chessBoard.push(row);
+    for(let j = 0; j < 8; j++){
+      if(whiteQueen[0] === i && whiteQueen[1] === j || blackQueen[0] === i && blackQueen[1] === j){
+        row.push(1);
+      } else {
+        row.push(0);
+      }
+    }
+  }
+  console.table(chessBoard);
+  return chessBoard;
+};
+
+let queenThreat = function(chessBoard){
+  let queenPosition = [];
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+      if(chessBoard[i][j] === 1){
+        queenPosition.push([i, j]);
+      }
+    }
+  }
+
+  let wQ = queenPosition[0];
+  let bQ = queenPosition[1];
+  
+  if(wQ[0] === bQ[0] || wQ[1] === bQ[1]){
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
 let whiteQueen = [0, 5];
 let blackQueen = [5, 0];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
